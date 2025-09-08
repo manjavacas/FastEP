4d3d81d459655a27556a427584c16e40fd713b9a
Antonio <antomanjavacas@gmail.com>
2025-09-08 14:50:18 +0200

    simplified dataset loading

--- FastEp/src/main.py
+++ FastEp/src/main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import polars as pl
+
 import torch
+import torch.nn as nn
 
 from numpy.lib.stride_tricks import sliding_window_view
 from pathlib import Path
@@ -11,27 +13,32 @@ DATA_DIR = Path(__file__).parent.parent / 'data'
 TRAIN_DATA_DIR = DATA_DIR / 'train'
 
 
-class SequenceLoader(Dataset):
+class SequenceDataset(Dataset):
     '''
-    Creates a dataset of sequences. Each sample is a sequence of features (states + actions) of length `seq_len`, and the target is the next partial state. 
+    Dataset of sequences. Each sample is a sequence of features (states + actions) of length `seq_len`, and the target is the next partial state.
     E.g. for seq_len=4:
         X = [x_0, x_1, x_2, x3],
         y = [y_4]
     Args:
         df: polars dataframe containing the data
-        state_cols: list of column names for the state variables
-        target_cols: list of column names for the target variables
         seq_len: length of the input sequences
     Outputs:
         self.X: tensor of shape [N, seq_len, features]
         self.y: tensor of shape [N, targets]
     '''
 
-    def __init__(self, df, state_cols, action_cols, target_cols, seq_len=4):
+    def __init__(self, df, seq_len=4):
         self.seq_len = seq_len
 
-        data_X = df.select(state_cols+action_cols).to_numpy()
-        data_y = df.select(target_cols).to_numpy()
+        cols = {prefix: [c for c in df.columns if c.startswith(prefix)]
+                for prefix in ['STATE_', 'ACTION_', 'NEXT_STATE_']}
+
+        self.state_cols = cols['STATE_']
+        self.action_cols = cols['ACTION_']
+        self.target_cols = cols['NEXT_STATE_']
+
+        data_X = df.select(self.state_cols + self.action_cols).to_numpy()
+        data_y = df.select(self.target_cols).to_numpy()
 
         seq_X = sliding_window_view(
             data_X, (seq_len, data_X.shape[1])).squeeze(axis=1).copy()
@@ -49,15 +56,13 @@ class SequenceLoader(Dataset):
 
 def main():
 
-    df = pl.read_csv(TRAIN_DATA_DIR / 'df_1' / 'df_1.csv')
+    # Load datasets...
+    df_names = [f.name for f in TRAIN_DATA_DIR.iterdir() if f.is_dir()]
+    datasets = []
+    for df_name in df_names:
+        df = SequenceDataset(pl.read_csv(
+            TRAIN_DATA_DIR / df_name / (df_name + '.csv')))
+        datasets.append(df)
 
-    cols = {prefix: [c for c in df.columns if c.startswith(prefix)]
-            for prefix in ["STATE_", "ACTION_", "NEXT_STATE_"]}
-
-    dataloader = SequenceLoader(
-        df, cols["STATE_"], cols["ACTION_"], cols["NEXT_STATE_"])
-
-    print(dataloader[:2])
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
